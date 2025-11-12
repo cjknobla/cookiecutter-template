187244ba2c7dd82039ceee3ef622a8c5c2580425
Chris Knoblauch <cjknobla@gmail.com>
2025-11-11 21:39:29 -0500

    lint final

--- tests/functional_tests/test__generate_project.py
+++ tests/functional_tests/test__generate_project.py
@@ -2,8 +2,5 @@ from pathlib import Path
 
 
 def test__can_generate_project(project_dir: Path):
-    """
-
-    execute: `cookiecutter <template directory> ...`
-    """
+    """execute: `cookiecutter <template directory> ...`."""
     assert project_dir.exists()
