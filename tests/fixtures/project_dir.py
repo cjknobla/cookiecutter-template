92995dc2959e25e1c96c61a20922b5e9071d00e5
Chris Knoblauch <cjknobla@gmail.com>
2025-11-11 20:16:56 -0500

    Parallel testing

--- tests/fixtures/project_dir.py
+++ tests/fixtures/project_dir.py
@@ -4,6 +4,7 @@ import subprocess
 from copy import deepcopy
 from pathlib import Path
 from typing import Dict
+from uuid import uuid4
 
 import pytest
 
@@ -15,13 +16,18 @@ from tests.utils.project import (
 
 @pytest.fixture(scope="session")
 def project_dir() -> Path:
+    test_session_id: str = generate_test_session_id()
     template_values = {
-        "repo_name": "test-repo",
+        "repo_name": f"test-repo-{test_session_id}",
     }
-    generated_repo_dir: Path = generate_project(template_values=template_values)
+    generated_repo_dir: Path = generate_project(template_values=template_values, test_session_id=test_session_id)
     try:
         initialize_git_repo(repo_dir=generated_repo_dir)
         subprocess.run(["make", "lint-ci"], cwd=generated_repo_dir, check=False)
         yield generated_repo_dir
     finally:
         shutil.rmtree(path=generated_repo_dir)
+
+def generate_test_session_id() -> str:
+    test_session_id = str(uuid4())[:6]
+    return test_session_id
